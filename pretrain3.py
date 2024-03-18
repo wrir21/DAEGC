a0c0d96de52d94e140cb0519e6f82fe0b76b0897
username <useremail@qq.com>
2024-03-18 22:33:06 +0800

    增加参数l

--- pretrain3.py
+++ pretrain3.py
@@ -40,7 +40,7 @@ def pretrain(dataset):
         model.train()
         A1_pred,A2_pred, z = model(x, adj, M)
         loss = F.binary_cross_entropy(A2_pred.view(-1), adj_label.view(-1))
-        loss = loss + args.p * torch.sum(abs(A1_pred-A2_pred))
+        loss = args.l*loss + args.p * torch.sum(abs(A1_pred-A2_pred))
 
         optimizer.zero_grad()
         loss.backward()
@@ -66,6 +66,7 @@ if __name__ == "__main__":
     parser.add_argument("--name", type=str, default="Citeseer")
     parser.add_argument("--max_epoch", type=int, default=100)
     parser.add_argument("--p", type=float, default=0.1)
+    parser.add_argument("--l", type=float, default=0.1)
     parser.add_argument("--lr", type=float, default=0.01)
     parser.add_argument("--n_clusters", default=6, type=int)
     parser.add_argument("--hidden_size", default=256, type=int)
@@ -87,6 +88,7 @@ if __name__ == "__main__":
         args.k = None
         args.n_clusters = 6
         args.p = 0.2
+        args.l = 0.02
         args.hidden_size = 256
         args.embedding_size = 24
     elif args.name == "Cora":
